2484b3ad2a87f324852bb901ec5c5cc8f2e2315c
mazen hoballah <53557473+mazenhoballah@users.noreply.github.com>
2024-11-05 16:09:37 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,39 @@ variable "region" {
   description = "The region to deploy Cloud Run"
   type        = string
 }
+
+variable "account_id" {
+  description = "The ID of the service account"
+  type        = string
+}
+
+variable "display_name" {
+  description = "The display name of the service account"
+  type        = string
+}
+
+
+variable "repository_id" {
+  description = "The ID of the Artifact Registry repository"
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the Artifact Registry repository"
+  type        = string
+}
+
+variable "format" {
+  description = "The format of the Artifact Registry repository"
+  type        = string
+}
+
+variable "docker_image_version" {
+  description = "The version of the Docker image"
+  type        = string
+}
+
+variable "image_name" {
+  description = "The name of the image"
+  type        = string
+}
