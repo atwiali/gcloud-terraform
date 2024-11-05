0ee99c613b71c2cf508474c230416846c7246205
mazen hoballah <53557473+mazenhoballah@users.noreply.github.com>
2024-11-05 16:09:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "google" {
 
 # Create a service account
 resource "google_service_account" "service_account" {
-  account_id   = "my-service-account"
-  display_name = "My Service Account"
+  account_id   = var.account_id
+  display_name = var.display_name
 }
 
 # Assign Cloud Run Admin role to the service account
@@ -30,21 +30,45 @@ resource "google_project_iam_binding" "storage_admin_role" {
 }
 
 # Create a service account key
+
 resource "google_service_account_key" "sa_key" {
   service_account_id = google_service_account.service_account.name
   public_key_type    = "TYPE_X509_PEM_FILE"
   private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
 }
 
+resource "google_artifact_registry_repository" "artifact_registry" {
+  location      = var.region
+  repository_id = var.repository_id
+  description   = var.description
+  format        = var.format
+}
+
+resource "null_resource" "docker_tag_push" {
+  triggers = {
+    always_run = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "sudo docker tag ${var.image_name} ${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}/${var.image_name}:${var.docker_image_version}"
+  }
+  provisioner "local-exec" {
+    command = "sudo docker push ${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}/${var.image_name}:${var.docker_image_version}"
+  }
+}
+
+
+
 # Deploy Cloud Run service
 resource "google_cloud_run_service" "cloud_run_service" {
   name     = "my-cloud-run-service"
   location = var.region
-// to be change the image
+  depends_on = [
+    null_resource.docker_tag_push
+  ]
   template {
     spec {
       containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repository_id}/${var.image_name}:${var.docker_image_version}"
       }
     }
   }
